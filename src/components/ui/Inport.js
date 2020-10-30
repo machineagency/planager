dd64a39f7259f8755141985860da2b4979596cc2
branchwelder <htwiggsmith@gmail.com>
2020-10-30 12:09:27 -1000

    New and improved data flow using proper react!

--- src/components/ui/Inport.js
+++ src/components/ui/Inport.js
@@ -5,16 +5,11 @@ import GlobalContext from "../../utils/GlobalContext";
 
 export default class Inport extends React.Component {
   static contextType = GlobalContext;
-
-  onClick() {
-    this.props.inportCallback(this.props);
-  }
-
   onDragOut(e) {
     const { global } = this.context;
     e.stopPropagation();
     e.persist();
-    global.startInportLink(e, this.props.id)
+    global.startInportLink(e, this.props.id);
   }
 
   render() {
@@ -25,12 +20,11 @@ export default class Inport extends React.Component {
             `Name: ` +
             this.props.name +
             `\n Value: ` +
-            String(this.props.dataVal.value)
+            String(this.props.data.value)
           }
           trigger={
             <button
               className="inport"
-              onClick={this.onClick.bind(this)}
               onMouseDown={this.onDragOut.bind(this)}
               data-id={this.props.id}
             />
