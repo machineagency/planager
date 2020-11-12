3c32f8a9ceb6980eb4337e855240d2bd86cfb52b
branchwelder <htwiggsmith@gmail.com>
2020-11-11 14:19:06 -1000

    Actions are now loaded together, not individually

---
+++ src/components/ActionLoader.js
@@ -0,0 +1,7 @@
+import Alert from "./actions/Alert";
+import Constant from "./actions/Constant";
+import LinearArray from "./actions/LinearArray";
+
+// In order for an action to show up on the main page, it must be imported
+// and exported here
+export { Alert, Constant, LinearArray };
