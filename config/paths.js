0dc3d4a96c6e5ee770a8e84991f626da36a728ce
branchwelder <htwiggsmith@gmail.com>
2020-11-13 13:10:50 -1000

    Clean up package.json

--- config/paths.js
+++ config/paths.js
@@ -1,11 +1,6 @@
-'use strict';
-
 const path = require('path');
 const fs = require('fs');
 const getPublicUrlOrPath = require('react-dev-utils/getPublicUrlOrPath');
-
-// Make sure any symlinks in the project folder are resolved:
-// https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
@@ -66,7 +61,5 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   publicUrlOrPath,
 };
-
-
 
 module.exports.moduleFileExtensions = moduleFileExtensions;
