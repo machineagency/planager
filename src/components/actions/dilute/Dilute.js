6103555ac0084a405b72e69cb856ba0679d0aaa7
branchwelder <htwiggsmith@gmail.com>
2021-01-22 15:32:16 -1000

    Yeast gates mock

--- src/components/actions/dilute/Dilute.js
+++ src/components/actions/dilute/Dilute.js
@@ -2,15 +2,13 @@ import React from "react";
 import GenericAction from "../GenericAction";
 import Inport from "../../base/Inport";
 import Outport from "../../base/Outport";
+import "./Dilute.css";
 
 export default class Dilute extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       inports: [
-        new Inport("Media", "type", "data", "Medi"),
-        new Inport("input OD", "type", "data", "input OD"),
-        new Inport("target OD", "type", "data", "target OD"),
         new Inport("strain in media", "type", "data", "strain in media"),
       ],
       outports: [
@@ -29,7 +27,41 @@ export default class Dilute extends React.Component {
         positionDeltas={this.props.positionDeltas}
       >
         <div className="actionTitle">Dilute</div>
-        <div className="actionContent">Content goes here!</div>
+        <div className="actionContent">
+          <div className="colonyContainer">
+            <span class="diluteLabel">Input OD:</span>
+            <input
+              type="text"
+              className="colonyInput"
+              placeholder="input OD"
+            />
+          </div>
+          <div className="colonyContainer">
+            <span class="diluteLabel">Target OD:</span>
+            <input
+              type="text"
+              className="colonyInput"
+              placeholder="target OD"
+            />
+          </div>
+          <div className="colonyContainer">
+            <span class="diluteLabel">Media:</span>
+            <input
+              type="text"
+              className="colonyInput"
+              placeholder="Media"
+            />
+          </div>
+
+          <div>
+            <div className="notesTitle">Notes</div>
+            <div className="notesContent">
+              96 flat 1 dilute (at constant volume (TX), OD defined average
+              volume (BF), OD defined well volume (GB) trying to hit about OD
+              600 0.0003, 0.00015, 0.000075) into 96 deep 1 with 1mL SC media
+            </div>
+          </div>
+        </div>
       </GenericAction>
     );
   }
--- src/components/actions/flowCytometryMeasurement/FlowCytometryMeasurement.js
+++ src/components/actions/flowCytometryMeasurement/FlowCytometryMeasurement.js
@@ -2,7 +2,7 @@ import React from "react";
 import GenericAction from "../GenericAction";
 import Inport from "../../base/Inport";
 import Outport from "../../base/Outport";
-import Signal from "../../base/Signal"
+import Signal from "../../base/Signal";
 
 export default class FlowCytometryMeasurement extends React.Component {
   constructor(props) {
@@ -10,11 +10,10 @@ export default class FlowCytometryMeasurement extends React.Component {
     this.state = {
       inports: [
         new Inport("signal", Signal, null, "Media: Media Type"),
-        new Inport("sample", "sample", null, "sample to be measured")
+        new Inport("sample", "sample", null, "sample to be measured"),
+        new Inport("OD", "Measured OD", null, "The OD that was measured"),
       ],
-      outports: [
-        new Outport("Cytometry", "cytometry data", null, "Cytometry"),
-      ],
+      outports: [new Outport("Cytometry", "cytometry data", null, "Cytometry")],
     };
   }
 
@@ -28,7 +27,22 @@ export default class FlowCytometryMeasurement extends React.Component {
         positionDeltas={this.props.positionDeltas}
       >
         <div className="actionTitle">FlowCytometryMeasurement</div>
-        <div className="actionContent">Content goes here!</div>
+        <div className="actionContent">
+          <div className="colonyContainer">
+            <span class="transferLabel">OD:</span>
+            <input
+              type="text"
+              className="colonyInput"
+              placeholder="Measured OD"
+            />
+          </div>
+          <div>
+            <div className="notesTitle">Notes</div>
+            <div className="notesContent">
+              96 flat 2 measure on flow quickly
+            </div>
+          </div>
+        </div>
       </GenericAction>
     );
   }
