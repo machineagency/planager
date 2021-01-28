5106155a9467b837aea5792d75ee1e004c68e337
branchwelder <htwiggsmith@gmail.com>
2021-01-28 09:35:29 -1000

    Actions are now layed out using CSS grid

--- src/components/actions/ActionLoader.js
+++ src/components/actions/ActionLoader.js
@@ -1,47 +1,47 @@
-// import Alert from "./alert/Alert";
-// import Constant from "./constant/Constant";
+import Alert from "./alert/Alert";
+import Constant from "./constant/Constant";
 // import JubileeDeck from "./jubileedeck/JubileeDeck";
 // import Wellplate from "./wellplate/Wellplate";
 // import ProtocolViewer from "./protocolViewer/ProtocolViewer";
 // import DownloadJSON from "./downloadJSON/DownloadJSON";
-// import Button from "./button/Button";
+import Button from "./button/Button";
 // import Counter from "./counter/Counter";
 // import SonicationPlan from "./sonicationPlan/SonicationPlan";
 // import AssignPlan from "./assignPlan/AssignPlan";
 
 // Harmonized Yeast gates
-import ProvisionPlate from "./provisionPlate/ProvisionPlate";
-import PatchPlate from "./patchPlate/PatchPlate";
-import Incubate from "./incubate/Incubate";
-import PickColonies from "./pickColonies/PickColonies";
-import MeasureOD from "./measureOD/MeasureOD";
-import Agitate from "./agitate/Agitate";
-import Dilute from "./dilute/Dilute";
-import Transfer from "./transfer/Transfer";
-import FlowCytometryMeasurement from "./flowCytometryMeasurement/FlowCytometryMeasurement";
+// import ProvisionPlate from "./provisionPlate/ProvisionPlate";
+// import PatchPlate from "./patchPlate/PatchPlate";
+// import Incubate from "./incubate/Incubate";
+// import PickColonies from "./pickColonies/PickColonies";
+// import MeasureOD from "./measureOD/MeasureOD";
+// import Agitate from "./agitate/Agitate";
+// import Dilute from "./dilute/Dilute";
+// import Transfer from "./transfer/Transfer";
+// import FlowCytometryMeasurement from "./flowCytometryMeasurement/FlowCytometryMeasurement";
 
 // In order for an action to show up on the main page, it must be imported
 // and exported here. This is much easier to manage than trying to import
 // them all in Main. We could also define subcategories here for
 // better organization of actions.
 export {
-  // Alert,
-  // Constant,
+  Alert,
+  Constant,
   // JubileeDeck,
   // Wellplate,
   // ProtocolViewer,
   // DownloadJSON,
-  // Button,
+  Button,
   // Counter,
   // SonicationPlan,
   // AssignPlan,
-  ProvisionPlate,
-  PatchPlate,
-  Incubate,
-  PickColonies,
-  MeasureOD,
-  Agitate,
-  Dilute,
-  Transfer,
-  FlowCytometryMeasurement,
+  // ProvisionPlate,
+  // PatchPlate,
+  // Incubate,
+  // PickColonies,
+  // MeasureOD,
+  // Agitate,
+  // Dilute,
+  // Transfer,
+  // FlowCytometryMeasurement,
 };
