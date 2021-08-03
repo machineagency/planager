8c786915a9c9ab8ad529419769d14876a5f04f2e
Hannah Twigg-Smith <htwiggsmith@gmail.com>
2021-08-03 15:51:44 -0700

    new python skeleton

---
+++ py/Action.py
@@ -0,0 +1,11 @@
+class Action:
+    def __init__(self):
+        self.name = None
+        self.outports = []
+        self.inports = []
+
+    def onReceive():
+        pass
+
+    def beforeSend():
+        pass
