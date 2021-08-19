e0c18e613602db4598720cefe861aefdb14841e0
Hannah Twigg-Smith <htwiggsmith@gmail.com>
2021-08-19 13:27:37 -0700

    Add Environment and workflow classes, reorganize.

--- py/workflow/Action.py
+++ py/workflow/Action.py
@@ -15,10 +15,19 @@ class Action:
         # I believe inports and outports should be an ordered collection of the appropriate classes. We want the order to remain the same, but a list might not be the right way of implementing this.
 
 
-        # Conditions for running the main loop of the action.
-        self.runCondition = None #how to do python enums?
+        # Conditions for running the main loop of the action and therefore refreshing output
+        self.updateMethod = None #how to do python enums?
         # types should be immediate, on signal, interval... is there a custom?
         # How is running the main loop of the action different from actions that have self-updating main loops? How to represent this?
+        # Actions should have explicit support for at least one update method.
+        # What if the global workspace could have a debug mode that switches all actions to "on intervention", which is like a debug step mode.
+        # Maybe read more about debuggers.
+
+        self.exportTo = None
+        # A list of file formats that this action can be exported to. Example: PAML
+        # Need to best figure out how to implement this. Maybe the export function should be at the workflow level?
+        # I think it should be at the action level though, then the actions are export format agnostic.
+
 
         # Agent setup and information.
         self.agent = None
@@ -45,6 +54,12 @@ class Action:
         # maybe this is not just boolean, instead is a enum that can support different modes?
         # people could then add different interaction support into their actions.
         # First thought: cli, node, notebook
+        # View modes will need to support actions, agents, panels, ports, inventory, logs
+        # Interfaces should have a default way of importing actions that maintains their functionality. 
+        # might default to limited CLI interface even in node/notebook. Actions
+        # can override this by having specific code for that display mode.
+        # An action could list the display modes it supports explicitly, which could be useful when exploring the action library.
+        # I want the interface to be able to list what actions can connect to eachother via the same input.
         
         # MORE THOUGHTS
         # - I want an easy testing framework. python unittest should be able to help.
@@ -54,6 +69,12 @@ class Action:
         # - How do we manage Python dependencies? Is there a way to indicate specific dependencies in this file?
         #     - I'm thinking that we actually have a requirements.txt file that comes with each action.
         # - Action installation? How is it going to work? 
+
+        self.panels = None
+        # What if panels were attached to the actions or ports that they represent?
+        # rename ports portals?
+
+        self.settings = None # dictionary of settings for this action instance. can be updated in the interface
 
     def onReceive(self):
         pass
@@ -74,10 +95,18 @@ class Action:
         print("this is the main function")
         return
 
-    def addToLog(self):
+    def appendToLog(self):
         # Writes a statement to the action log.
         pass
 
     def info(self):
         print(self.name, self.outports, self.inports)
         return
+
+    def export(self):
+        #Export this action to the selected format.
+        pass
+
+    def save(self):
+        # this function is for saving this instance of the action to the workflow file in json
+        pass
