a8100588485e11397b30fa52f5012e7ebb0d270f
branchwelder <htwiggsmith@gmail.com>
2021-09-03 16:47:58 -0700

    Add favicons and fix cookies

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ app.register_blueprint(paml_blueprint, url_prefix="/paml")
 # NOTE: The secret key is used to cryptographically-sign the cookies used for storing
 #       the session identifier.
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
 # Configure Redis for storing the session data on the server-side
 app.config["SESSION_TYPE"] = "redis"
@@ -29,5 +29,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_USE_SIGNER"] = True
 app.config["SESSION_REDIS"] = redis.from_url(redis_url)
 
-# Create and initialize the Flask-Session object. Allows for server-side sessions, rather than Flask's default client-side sessions
+# Create and initialize the Flask-Session object. Flask-Session is a flask
+# extension that adds support for server-side sessions, rather than Flask's 
+# default client-side sessions. Don't ever access the Session object
+# directly; just use the built in Flask session interface.
+app.config["SESSION_COOKIE_SECURE"] = True
 server_session = Session(app)
