881a23b3fecb5a9397e5fc24ae4b9a9322afe308
branchwelder <htwiggsmith@gmail.com>
2021-12-09 21:12:41 -0800

    git literally ignores case changes what the fuck

---
+++ app/planager/actionsets/axidraw/options/Options.py
@@ -0,0 +1,16 @@
+from app.planager.workflow.Action import Action
+
+CONFIG = {
+    "displayName": "Axidraw Options",
+    "inports": {},
+    "outports": {"options": {"displayName": "output options", "description": ""}},
+}
+
+
+class Options(Action):
+    def __init__(self):
+        Action.__init__(self, CONFIG)
+
+    def main(self):
+        """The main loop; this is what runs when the action is run."""
+        pass
