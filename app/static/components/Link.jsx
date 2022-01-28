73c9401a3c55790afd605fffad9dc12f1df4018e
branchwelder <htwiggsmith@gmail.com>
2022-01-28 00:32:01 -0800

    Add animation to link svg

--- app/static/components/Link.jsx
+++ app/static/components/Link.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import GradientPath from "gradient-path";
 import "./styles/link.css";
 
 export default function Link({ startX, startY, endX, endY, removeLink, id }) {
+  const linkPath = React.useRef(null);
+  let [hovering, setHovering] = React.useState(false);
+
   function calculateBezier() {
     return `M${startX},${startY}
       C${startX + 100},${startY}
@@ -15,15 +19,37 @@ export default function Link({ startX, startY, endX, endY, removeLink, id }) {
   return (
     <div>
       <svg className='wire'>
-        <g id='component'>
-          <path
-            className='linkPath'
-            fill='none'
-            strokeWidth='2'
-            d={calculateBezier()}
-            onContextMenu={remove}
+        <path
+          className='backgroundPath'
+          d={calculateBezier()}
+          onMouseOver={() => setHovering(true)}
+          onMouseOut={() => setHovering(false)}
+        />
+        <path
+          id='wirePath'
+          ref={linkPath}
+          className='linkPath'
+          d={calculateBezier()}
+          onContextMenu={remove}
+        />
+        <circle id='droplet'>
+          <animateMotion
+            dur='2s'
+            repeatCount='indefinite'
+            calcMode='spline'
+            keySplines='0.4 0 0.2 1'
+            keyTimes='0; 1'>
+            <mpath xlinkHref='#wirePath' />
+          </animateMotion>
+          <animate
+            attributeType='XML'
+            attributeName='fill'
+            from='#2aa198'
+            to='#6c71c4'
+            dur='2s'
+            repeatCount='indefinite'
           />
-        </g>
+        </circle>
       </svg>
     </div>
   );
