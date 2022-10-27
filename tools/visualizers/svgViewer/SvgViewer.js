2d3d02f25d96095668a91d29d39a36a1a9580c2e
hannah <htwiggsmith@gmail.com>
2022-10-27 10:57:50 -0700

    Better svg viewer

--- tools/visualizers/svgViewer/SvgViewer.js
+++ tools/visualizers/svgViewer/SvgViewer.js
@@ -1,21 +1,23 @@
 import { html, css } from "lit";
 import { Tool } from "../../../src/components/tool_ui/Tool";
-import { select } from "d3";
 
 export default class SvgViewer extends Tool {
   static styles = css`
-    #vizContainer {
+    #imContainer {
+      width: 40rem;
+      resize: horizontal;
+      overflow: auto;
+    }
+    #im {
+      width: 100%;
     }
   `;
 
-  updateSVG() {
-    let container = select(this.shadowRoot.querySelector("#vizContainer"));
-    container.selectAll("svg").remove();
-    container.html(this.inports.svg);
-  }
-
   render() {
-    this.updateSVG();
-    return html`<div id="vizContainer"></div>`;
+    return html`<div id="imContainer">
+      <img
+        id="im"
+        src="data:image/svg+xml;base64,${window.btoa(this.inports.svg)}" />
+    </div>`;
   }
 }
